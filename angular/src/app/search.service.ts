6cfa4e635dbbb14689f7aa5c7c2dc9a45df46b6b
Graham Towse <graham.towse@meeco.me>
2016-11-27 09:51:04 +1100

    Angular provider search service WIP

--- angular/src/app/search.service.ts
+++ angular/src/app/search.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Headers, RequestOptions } from '@angular/http';
+
+export interface Provider {
+    name: string;
+    address?: {
+        street?: string;
+        postcode?: string;
+    }
+}
 
 @Injectable()
 export class SearchService {
 
-  constructor() { }
+  searchUrl:string;
+
+  constructor() {
+    this.searchUrl = ;
+  }
+
+  findProviders (postcode: string, clusterType:string): Observable<Provider[]> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+
+    return this.http.post(this.searchUrl, { postcode: postcode, cluster: clusterType }, options)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
 
 }
