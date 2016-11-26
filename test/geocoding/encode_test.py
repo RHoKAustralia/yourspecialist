2688fa63b2eced9ba52153d62a837c60f93c2254
Lea Anthony <lea.anthony@apexdatasolutions.net>
2016-11-26 13:55:04 +1100

    Geo En/Decoder Library
    To be used for data cleanup.

---
+++ test/geocoding/encode_test.py
@@ -0,0 +1,21 @@
+
+import geo, os
+
+address = """
+10 OBrien Street
+CESSNOCK
+NSW
+2325
+"""
+
+# Ensure you set this variable in your environment
+API_KEY = os.environ['GOOGLE_API_KEY']
+
+myEncoder = geo.Geo(API_KEY)
+
+print(myEncoder.geo_from_address(address))
+
+lat = -32.8418769
+lng = 151.345344
+
+print(myEncoder.address_from_geo(lat,lng))
---
+++ test/geocoding/geo.py
@@ -0,0 +1,50 @@
+#
+# Geo En/Decoder Helper object
+# Lea Anthony (https://github.com/lea-anthony)
+# Donated as part of Sydney RHoK Summer Hackathon
+#
+
+from geolocation.main import GoogleMaps
+import json
+
+
+class Geo:
+
+    def __init__(self, API_KEY):
+        self.google_maps = GoogleMaps(api_key=API_KEY)
+
+    def processLocation(self, location):
+        """Converts the given Address object into JSON"""
+        temp = location.__dict__
+        aa = temp['_administrative_area']
+        del temp['_administrative_area']
+        admin_areas = []
+        for admin_area in aa:
+            this_admin = {'type': admin_area.area_type, 'name': admin_area.name,
+                          'short_name': admin_area.short_name}
+            admin_areas.append(this_admin)
+        temp["administrative_areas"] = admin_areas
+        return json.dumps(temp)
+
+    def geo_from_address(self, address):
+        """Gets full address details for the given address string. Returns JSON data"""
+        locations = self.google_maps.search(location=address)  # sends search to Google Maps.
+
+        result = []
+        for location in locations.all():
+            this_result = self.processLocation(location)
+            result.append(this_result)
+
+        return result
+
+    def address_from_geo(self, lat, lng):
+        """Gets full address details for the given long/lat location. Returns JSON data"""
+        locations = self.google_maps.search(lat=lat, lng=lng).all()  # sends search to Google Maps.
+
+        result = []
+        for location in locations:
+            this_result = self.processLocation(location)
+            result.append(this_result)
+
+        return result
+
