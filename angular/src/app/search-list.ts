b6d42cc27812d740f09ef98a0b10999c9a65bfe5
Alvin Tolentino <alvin@lifeiq.net>
2016-11-27 12:48:37 +1100

    incremental commit

--- angular/src/app/search-list.ts
+++ angular/src/app/search-list.ts
@@ -1,6 +1,5 @@
 import { Component } from 'angular2/core';
 import { SearchService } from './search.service';
-import { SearchItemRenderer } from'./search-item-renderer.ts';
 import { SearchPipe } from './search-form.pipe';
 
 @Componet({
