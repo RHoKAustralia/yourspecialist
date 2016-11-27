16d018925f6db004f0db185e2d78f2a3d5135b48
Lea Anthony <lea.anthony@apexdatasolutions.net>
2016-11-27 13:30:33 +1100

    Second Geo fix for data

--- libs/geocoding/fixup_location.py
+++ libs/geocoding/fixup_location.py
@@ -9,7 +9,7 @@ with open('geoproviders.json') as data_file:
     for provider in providers:
         if len(provider['geo']['data']) > 0:
             provider['geo']['data'] = provider['geo']['data'][0]
-            provider['geo']['data']['location'] = [float(provider['geo']['data']['lng']), float(provider['geo']['data']['lat'])]
+            provider['geo']['data']['loc'] = { "type" : "Point", "coordinates": [float(provider['geo']['data']['lng']), float(provider['geo']['data']['lat'])]}
         result.append(provider)
 
 f.write(json.dumps(result))
